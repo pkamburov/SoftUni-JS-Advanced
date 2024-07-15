3847c08a2429c223d32f4df607fa314c297475c9
Pavel Kamburov <pkamburov89@gmail.com>
2024-07-15 19:51:07 +0300

    Initial commit

---
+++ 04-DOM-Introduction-Exercise/subtraction.js
@@ -0,0 +1,6 @@
+function solve() {
+    let num1 = document.getElementById('firstNumber').value;
+    let num2 = document.getElementById('secondNumber').value;
+    let result = Number(num1) - Number(num2);
+    document.getElementById('result').textContent = result;
+}
